f1ba81e6346d50bb9ed24b6d7024838cf9babc68
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2025-03-12 18:51:36 +0400

    added dropDown menu UI

--- src/pages/home/view/index.tsx
+++ src/pages/home/view/index.tsx
@@ -1,8 +1,12 @@
+import DropdownMenu from "../components/dropDown";
+
+
 
 const Home = () => {
   return (
     <div>
-        <p className="text-[34px]">დავალებების გვერდი</p>
+        <p className="text-[34px] font-semibold">დავალებების გვერდი</p>
+        <DropdownMenu/>
     </div>
   )
 }
